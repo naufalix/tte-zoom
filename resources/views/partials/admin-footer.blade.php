3aefc5557609cb7d68b720afbe4ee8cc48071d52
naufalix <naufalulinnuha40@gmail.com>
2026-03-18 15:31:24 +0700

    view: update year

--- resources/views/partials/admin-footer.blade.php
+++ resources/views/partials/admin-footer.blade.php
@@ -3,7 +3,7 @@
   <div class="container d-flex flex-column flex-md-row align-items-center justify-content-between">
     <!--begin::Copyright-->
     <div class="text-dark order-2 order-md-1">
-      <span class="text-muted fw-bold me-1">2024©</span>
+      <span class="text-muted fw-bold me-1">{{ date('Y') }}©</span>
       <a href="https://naufal.dev" target="_blank" class="text-gray-800 text-hover-primary">Kominfo Kabupaten Malang</a>
     </div>
     <!--end::Copyright-->
